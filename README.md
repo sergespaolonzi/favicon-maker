a8db0fbf4b85e2f497bdc37c8c1adcd825cf2e43
Serge Spaolonzi <sergio351393@gmail.com>
2026-02-03 21:38:02 -0300

    Commit

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ Favicon maker.
 
 
 # Install
-
 sudo dnf install potrace ImageMagick
-
 gem install minitest
+gem install mini_magick
 
+# Usage
+ruby -Ilib ./bin/favicon_maker --input /tmp/input/icon.png --outputdir /tmp/favicon/
--- lib/favicon_maker.rb
+++ lib/favicon_maker.rb
@@ -3,22 +3,20 @@ require "favicon_maker/txtlib"
 
 
 module FaviconMaker
-   def self.run(input, outputdir)
-      
-      current_folder = File.expand_path('../lib', File.dirname(__FILE__))
-      input_file = File.join(current_folder, "image.png")
-      output_dir = File.join(current_folder, "output.png")
-      
-      Imagelib.make_ico(input_file, output_dir)
-      Imagelib.make_pngs(input_file, output_dir)
-      Imagelib.make_svg(input_file, output_dir)
-      Txtlib.make_html(output_dir)
-
+   def self.run(input, output_dir)
 
       name = "App"
       short_name = "App" 
       theme_color = "#ffffff"
       background_color = "#ffffff"
+
+      Imagelib.make_ico(input, output_dir)
+      Imagelib.make_pngs(input, output_dir)
+      Imagelib.make_svg(input, output_dir)
+      Txtlib.make_html(name, output_dir)
+
+
+
       Txtlib.make_webappmanifest(name, short_name, theme_color, background_color, output_dir)
    end
 end
